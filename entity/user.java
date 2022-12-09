d7492a8f155ee5febce2f6bb2fdc2dd796d02416
tianhao202 <120072807+tianhao202@users.noreply.github.com>
2022-12-08 20:19:36 -0700

    Add files via upload

---
+++ entity/user.java
@@ -0,0 +1,72 @@
+package entity;
+
+public class user implements java.io.Serializable {
+
+
+	private Integer id;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	private String username;
+	public String getUsername() {
+		return this.username;
+	}
+	public void setName(String username) {
+		this.username = username;
+	}
+	private String password;
+	public String getPassword() {
+		return this.password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	private String name;
+	public String getName() {
+		return this.name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	private String age;
+	public String getAge() {
+		return this.age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	private String gender;
+	public String getGender() {
+		return this.gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	private String cardnumber;
+	public String getCardNumber() {
+		return this.cardnumber;
+	}
+	public void setCardNumber(String cardnumber) {
+		this.cardnumber = cardnumber;
+	}
+	private String cardpassword;
+	public String getCardPassword() {
+		return this.cardpassword;
+	}
+	public void setCardPassword(String cardpassword) {
+		this.cardpassword = cardpassword;
+	}
+	private String balance;
+	public String getBalance() {
+		return this.balance;
+	}
+	public void setBalance(String balance) {
+		this.balance = balance;
+	}
+	
+
+}
